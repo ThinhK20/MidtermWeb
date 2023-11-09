155dff99aee8eb4e7351a79d4c8b6fc55302fb05
huyhoanFithcmus <huyhoan652002@gmail.com>
2023-11-09 09:26:11 +0700

    change login signup UI

--- client/src/components/Login/login.tsx
+++ client/src/components/Login/login.tsx
@@ -15,11 +15,10 @@ export default function Login() {
             className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
           >
             <img
-              className="w-8 h-8 mr-2"
-              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
-              alt="logo"
+              src="http://www.nicdarkthemes.com/themes/travel/wp/demo/love-travel/wp-content/uploads/sites/3/2018/11/logo.png"
+              alt=""
+              className="header-logo__img"
             />
-            Flowbite
           </a>
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
